5d6c61c128d5ec1ce33d03802ff0f8d0b545b89a
Debabrata Mandal <bapidebabrata@gmail.com>
2022-05-09 16:57:22 +0530

    Update HTML, CSS and JavaScript for the mobile menu

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 function showTheMenu() {
   document.getElementById('overlay').style.display = 'flex';
+  document.getElementById('overlay-bg').style.display = 'block';
+  document.getElementById('header').style.filter = 'blur(6px)';
+  document.getElementById('profile').style.filter = 'blur(6px)';
+  document.getElementById('projects').style.filter = 'blur(6px)';
 }
 
 function hideTheMenu() {
   document.getElementById('overlay').style.display = 'none';
+  document.getElementById('overlay-bg').style.display = 'none';
+  document.getElementById('header').style.filter = 'blur(0px)';
+  document.getElementById('profile').style.filter = 'blur(0px)';
+  document.getElementById('projects').style.filter = 'blur(0px)';
 }
 
 const navElement = document.getElementById('nav');
