158025d7164a09cdf2263da38750117a3759aa99
Terbeche Mustapha <termus96@gmail.com>
2022-05-09 09:55:11 +0100

    add style and js to the overlay menu

--- scripts.js
+++ scripts.js
@@ -2,5 +2,17 @@ const element = document.getElementById("nav");
 element.addEventListener("click", showTheMenu);
 
 function showTheMenu() {
-    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overlay").style.display = "flex";
   }
+
+  const close_button = document.getElementById("close-button");
+  close_button.addEventListener("click", hideTheMenu);
+  function hideTheMenu() {
+      document.getElementById("overlay").style.display = "none";
+    }
+
+
+  const menu_links = document.getElementsByClassName("menu-link");
+    for(let i=0;i < menu_links.length ;i++){
+      menu_links[i].addEventListener("click", hideTheMenu);
+    }
