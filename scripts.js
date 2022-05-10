5d3cd3867bbe1fd46b241935eaca4a62df574fb9
Debabrata Mandal <bapidebabrata@gmail.com>
2022-05-10 15:23:51 +0530

    Modify html and Add js to create projects section dynamically

--- scripts.js
+++ scripts.js
@@ -24,3 +24,95 @@ const menuLinks = document.getElementsByClassName('menu-link');
 for (let i = 0; i < menuLinks.length; i += 1) {
   menuLinks[i].addEventListener('click', hideTheMenu);
 }
+
+let projectsDetails = [
+  {
+    title : "Tonic",
+    info : ["CANOPY", "Back End Dev", "2015"],
+    featured_image : "./images/project-snapshots/project1.png",
+    description : "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    technologies : ["html", "css", "javascript"],
+    link_to_Source : "#",
+    link_to_live_version : "#"
+  },
+  {
+    title : "Tonic",
+    info : ["CANOPY", "Back End Dev", "2015"],
+    featured_image : "./images/project-snapshots/project1.png",
+    description : "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    technologies : ["html", "css", "javascript"],
+    link_to_Source : "#",
+    link_to_live_version : "#"
+  },
+  {
+    title : "Tonic",
+    info : ["CANOPY", "Back End Dev", "2015"],
+    featured_image : "./images/project-snapshots/project1.png",
+    description : "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    technologies : ["html", "css", "javascript"],
+    link_to_Source : "#",
+    link_to_live_version : "#"
+  },
+  {
+    title : "Tonic",
+    info : ["CANOPY", "Back End Dev", "2015"],
+    featured_image : "./images/project-snapshots/project1.png",
+    description : "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    technologies : ["html", "css", "javascript"],
+    link_to_Source : "#",
+    link_to_live_version : "#"
+  }
+]
+
+for (let i=0; i<projectsDetails.length; i+=1) {
+  let projectSection = document.createElement('section');
+  projectSection.className = 'project';
+
+  let projectTitle = document.createElement('h3');
+  projectTitle.className = 'project-heading';
+  projectTitle.textContent = projectsDetails[i].title;
+  projectSection.appendChild(projectTitle);
+
+  let projectInfo = document.createElement('ul');
+  projectInfo.className = 'project-info';
+  
+  let projectName = document.createElement('li');
+  projectName.className = 'project-name';
+  projectName.textContent = projectsDetails[i].info[0];
+  projectInfo.appendChild(projectName);
+  
+  let projectRole = document.createElement('li');
+  projectRole.className = 'project-role';
+  projectRole.textContent = projectsDetails[i].info[1];
+  projectInfo.appendChild(projectRole);
+  
+  let projectYear = document.createElement('li');
+  projectYear.className = 'project-year';
+  projectYear.textContent = projectsDetails[i].info[2];
+  projectInfo.appendChild(projectYear);
+  
+  projectSection.appendChild(projectInfo);
+  
+  
+/*
+<section class="project">
+        <img class="project-snapshot" src="./images/project-snapshots/project1.png" alt="project snapshot">
+        <p class="project-description">
+          A daily selection of privately personalized reads; no accounts or
+          sign-ups required.
+        </p>
+        <ul class="project-languages">
+          <li>html</li>
+          <li>css</li>
+          <li>javascript</li>
+        </ul>
+        <button type="button">See Project</button>
+      </section>
+*/
+
+
+document.querySelector("#projects").appendChild(projectSection);
+
+
+} 
+
